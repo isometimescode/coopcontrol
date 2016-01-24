09bfff8fa33d543215e6c4c540ce609e5b1786af
Toni Wells <isometimescode@users.noreply.github.com>
2016-01-23 17:49:45 -0800

    update door to use wiringpi library

--- door.py
+++ door.py
@@ -5,33 +5,52 @@
 # whenever it receives power. Because of this, we can't have the power on all the time; it
 # needs to be switched on and off again when the motor should be run.
 #
+# My crontab: runs every 15 minutes every day between early morning and a possible sunset,
+# which is just an estimated based on the widest range for my area's long summer days.
+#
+# */15 6-23 * * * python /full/path/door.py
+#
 
 import json
 import sys
+from time import sleep
 from datetime import datetime
 from dateutil import parser, tz
+import wiringpi2 as wiringpi
 import coop_settings
 
-# function for managing the door open/close state
-def changedoor(newstate):
-    "This changes the door state to open or closed"
-    if newstate == 1:
-        print 'door is opening'
-    else:
-        print 'door is closing'
-    with open(coop_settings.doorstate_file,'w') as f:
-        f.write(str(newstate))
-    f.close
 
-# check the door's current state
-# TODO: replace this with a contact switch
-try:
-    with open(coop_settings.doorstate_file,'r') as f:
-        dooropen = f.readline()
-        dooropen = dooropen.strip()
-    f.close
-except IOError:
-    dooropen = 0
+# function for managing the door open/close state
+# technically, we don't know if the door is already opened or closed
+# this function just turns the motor for half a second regardless
+def changedoor(newstate):
+    wiringpi.wiringPiSetup()
+    wiringpi.pinMode(coop_settings.pin_door, 1) # output mode
+
+    if newstate == 1: #open
+        coop_settings.appendLog('door','opening the door')
+    else:
+        coop_settings.appendLog('door','closing the door')
+
+    # low turns the motor on
+    wiringpi.digitalWrite(coop_settings.pin_door, 0)
+    # wait for it to finish running - a bigger door would require more to complete
+    sleep(0.5)
+    # high turns the motor back off so that it can run again later
+    wiringpi.digitalWrite(coop_settings.pin_door, 1)
+
+# check the door's current state; we are just manually saving this data to a file
+# since there is no other way to know if the door was last opened or closed
+# TODO: replace this with a contact switch to get real data
+def getdoor():
+    try:
+        with open(coop_settings.doorstate_file,'r') as f:
+            doorstate = f.readline()
+            doorstate = doorstate.strip()
+        f.close
+    except IOError:
+        doorstate = 0
+    return doorstate
 
 # get sunrise/sunset data
 srdata = coop_settings.getJSONData(coop_settings.sunset_file)
@@ -49,14 +68,10 @@ if sunrise.date() != current.date():
 # the door is only open during sunrise and sunset
 # any other time the door should be closed
 if sunrise <= current < sunset:
-    print "its daytime"
-    if int(dooropen) == 0:
+    coop_settings.appendLog('door','its after sunrise('+sunrise.strftime('%X')+') check door: open')
+    if int(getdoor()) == 0:
         changedoor(1)
-    else:
-        print "door is already open"
 else:
-    print "its nighttime"
-    if int(dooropen) == 1:
+    coop_settings.appendLog('door','its after sunset('+sunset.strftime('%X')+') check door: closed')
+    if int(getdoor()) == 1:
         changedoor(0)
-    else:
-        print "door is already closed"
